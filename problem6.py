5fd036c6950daefcf0c8f1e8b266781e8b5b26a0
Raghaddahi <raghaddahi27@gmail.com>
2025-02-23 00:15:05 +0200

    Delete problem6.py

--- problem6.py
+++
@@ -1,9 +0,0 @@
-total=0
-num=int(input())
-for i in range(1,num):
-    if num%i==0:
-        total+=i
-if total==num:
-    print(f"{num} is a perfect number")
-else:
-    print(f"{num} is not a perfect number")
