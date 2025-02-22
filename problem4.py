937af52b625c6f11a0fcd54a49e1957239e85197
Raghaddahi <raghaddahi27@gmail.com>
2025-02-23 00:14:58 +0200

    Delete problem4.py

--- problem4.py
+++
@@ -1,7 +0,0 @@
-sentence=input()
-words= sentence.split()
-reversed_words=[]
-for i in words:
-    reversed_words.append(i[::-1])
-reversed_sentence=" ".join(reversed_words)
-print(reversed_sentence)
